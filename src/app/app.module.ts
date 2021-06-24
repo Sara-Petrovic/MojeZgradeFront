8b43bd3c53daaa71eabf0703b77ebf4c2b279730
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-06-24 16:14:01 +0200

    Kreirana loginsuccess stranica i rutiranje do nje.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { LoginsuccessComponent } from './loginsuccess/loginsuccess.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginsuccessComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { data } from 'jquery';
 import { User } from '../model/user';
 import { RegistrationService } from '../services/registration.service';
@@ -11,16 +13,23 @@ import { RegistrationService } from '../services/registration.service';
 })
 export class LoginComponent implements OnInit {
   user = new User();
+  msg = '';
 
-  constructor(private _service: RegistrationService) { }
+  constructor(private _service: RegistrationService, private _router: Router) { }
 
   ngOnInit(): void {
   }
 
-  loginUser(){
+  loginUser() {
     this._service.loginUserFromRemote(this.user).subscribe(
-      data=> console.log("response received") ,
-      error=>console.log("exception occured")
+      data => {
+        console.log("response received");
+        this._router.navigate(['/loginsuccess'])
+      },
+      error => {
+        console.log("exception occured");
+        this.msg="Pogresan email i/ili lozinka. Molimo pokusajte ponovo.";
+      }
     );
   }
 
