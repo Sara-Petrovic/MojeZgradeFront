69190f2fcc9a84fc6dcfad86b6b9d4ae784533e6
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-06-30 19:31:47 +0200

    Napravljena komponenta za detaljan prikaz sednice

--- src/app/sednica-details/sednica-details.component.ts
+++ src/app/sednica-details/sednica-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Mesto } from '../model/mesto';
+import { SednicaSkupstine } from '../model/sednica-skupstine';
+import { StambenaZajednica } from '../model/stambena-zajednica';
+import { VlasnikPosebnogDela } from '../model/vlasnik-posebnog-dela';
+import { SednicaSkupstineService } from '../services/sednica-skupstine.service';
 
 @Component({
   selector: 'app-sednica-details',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SednicaDetailsComponent implements OnInit {
 
-  constructor() { }
+  sednicaId: number;
+  sednica: SednicaSkupstine;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private router: Router,
+    private sednicaService: SednicaSkupstineService) { }
+
+  ngOnInit() {
+    this.sednica = new SednicaSkupstine();
+    this.sednica.stambenaZajednica = new StambenaZajednica();
+    this.sednica.stambenaZajednica.mesto = new Mesto(0,"","");
+    this.sednicaId = this.route.snapshot.params['id'];
+    this.sednica.vlasnici = new Array();
+
+    this.sednicaService.getSednicaSkupstineFromRemote(this.sednicaId)
+      .subscribe(data => {
+        console.log(data)
+        this.sednica = data;
+
+      }, error => console.log(error));
+  }
+
+  list() {
+    this.router.navigate(['sednice']);
   }
 
 }
