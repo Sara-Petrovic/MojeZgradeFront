bbb2ee03b8f2c8d0d5b58e1369ef29c5a71eba72
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2021-08-06 17:06:15 +0200

    Izmenjen nacin azuriranja stavki racuna

--- src/app/pages/racun-details/racun-details.component.ts
+++ src/app/pages/racun-details/racun-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Racun } from 'src/app/model/racun';
+import { StavkaRacuna } from 'src/app/model/stavka-racuna';
+import { Usluga } from 'src/app/model/usluga';
 import { RacunService } from 'src/app/services/racun.service';
+import { UslugaService } from 'src/app/services/usluga.service';
 
 @Component({
   selector: 'app-racun-details',
@@ -12,9 +15,20 @@ export class RacunDetailsComponent implements OnInit {
   racunId!: number;
   racun!: Racun;
 
+  sveUsluge!: Usluga[];
+  selectedUsluga!: Usluga;
+
+  kolicina: number;
+  ukupnaVrednost: number;
+
   constructor(private route: ActivatedRoute,
     private racunService: RacunService,
-    private router: Router) { }
+    private uslugeService: UslugaService,
+    private router: Router) {
+    this.kolicina = 1;
+    this.ukupnaVrednost = 0;
+
+  }
 
   ngOnInit(): void {
     this.racun = new Racun();
@@ -23,6 +37,12 @@ export class RacunDetailsComponent implements OnInit {
       .subscribe(data => {
         this.racun = data;
       }, error => console.log(error));
+      this.fillComboBoxUsluge();
+  }
+
+  fillComboBoxUsluge(): void {
+    this.uslugeService.getAllUslugeFromRemote()
+      .subscribe(sveUsluge => { this.sveUsluge = sveUsluge; console.log(sveUsluge) });
   }
 
   list() {
@@ -52,10 +72,9 @@ export class RacunDetailsComponent implements OnInit {
     );
 
     alert("Status racuna je azuriran na placen.");
-    // this.router.navigate(['moje-zgrade/racuni']);
   }
 
-  racunIsSent(){
+  racunIsSent() {
     if (this.racun.status != 'KREIRAN') {
       alert("Ne mozete da posaljete ovaj racun.");
       return;
@@ -65,4 +84,25 @@ export class RacunDetailsComponent implements OnInit {
       data => { console.log(data); this.racun.status = "POSLAT" }
     );
   }
+
+  addStavka() {
+    if (!this.isNumber(this.kolicina) || this.kolicina <= 0) {
+      alert("Kolicina mora da bude pozitivan broj.");
+    }
+    this.racun.stavke.push(new StavkaRacuna(this.racun.stavke.length, this.selectedUsluga.cena, this.kolicina, this.selectedUsluga, new Racun()));
+    this.ukupnaVrednost += this.kolicina * this.selectedUsluga.cena;
+  }
+
+  isNumber(value: string | number): boolean {
+    return ((value != null) &&
+      (value !== '') &&
+      !isNaN(Number(value.toString())));
+  }
+
+  deleteStavka(stavka:StavkaRacuna){
+    let index = this.racun.stavke.indexOf(stavka);
+    this.racun.stavke.splice(index, 1);
+    console.log(this.racun.stavke);
+  }
+
 }
